bf7293bb6c599523d68f7f73f8dfe6210da2d1ca
Lidiya <simutina.1994@mail.ru>
2024-07-14 21:33:53 +0300

    add branch number-experiments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int amount = firstNumber + secondNumber;
+        System.out.println("Сумма: " + amount);
+
+        int difference = firstNumber - secondNumber;
+        System.out.println("Разность: " + difference);
+
+        int multiplying = firstNumber * secondNumber;
+        System.out.println("Произведение чисел: " + multiplying);
+
+        double division = (double) firstNumber / secondNumber;
+        System.out.println("Частное чисел: " + division);
+
     }
 }
